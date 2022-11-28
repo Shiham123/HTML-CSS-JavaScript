b9136d84f4616bb1e4f4d099df4250e03d07c946
unknown <s36651786@gmail.com>
2022-11-28 18:33:07 +0600

    img slider

---
+++ JavaScript projects (codeWithHossain)/Progress bar/main.js
@@ -0,0 +1,21 @@
+const progressBars = document.querySelectorAll('.column');
+
+for (let progressBar of progressBars) {
+  const circle = progressBar.querySelector('.circle');
+  const percent = progressBar.querySelector('.percent');
+  const progress = progressBar.querySelector('.progress');
+
+  let progressed = 0;
+
+  const startProgress = () => {
+    progressed += 1;
+
+    if (progressed <= progressBar.getAttribute('data-percent')) {
+      circle.style.top = progressed + '%';
+      progress.style.height = progressed + '%';
+      percent.innerHTML = progressed + '%';
+    }
+    requestAnimationFrame(startProgress);
+  };
+  startProgress();
+}
