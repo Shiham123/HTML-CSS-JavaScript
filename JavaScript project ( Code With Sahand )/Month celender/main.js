0dfc939b6a13292f32a702309897917420e60770
unknown <s36651786@gmail.com>
2022-12-18 06:52:05 +0600

    all fix

--- JavaScript project ( Code With Sahand )/Month celender/main.js
+++ JavaScript project ( Code With Sahand )/Month celender/main.js
@@ -1,12 +1,14 @@
 const monthElement = document.querySelector('.date'),
   eachMonth = monthElement.querySelector('h1'),
-  eachDate = monthElement.querySelector('p');
+  eachDate = monthElement.querySelector('p'),
+  dayElement = document.querySelector('.days');
 
-const monthIndex = new Date().getMonth(),
-  lastDay = new Date(new Date().getFullYear(), monthIndex + 1, 0);
+const monthIndex = new Date().getMonth();
+const lastDay = new Date(new Date().getFullYear(), monthIndex, 0).getDate();
+const firstDay = new Date(new Date().getFullYear(), monthIndex, 1).getDay();
+console.log(firstDay);
 
 let dayIndex = new Date().toDateString();
-console.log(lastDay);
 
 const months = [
   'January',
@@ -25,3 +27,19 @@ const months = [
 
 eachMonth.innerText = months[monthIndex];
 eachDate.innerText = dayIndex;
+
+let days = '';
+
+for (let i = firstDay; i > 0; i--) {
+  days += `<div class="empty"></div>`;
+}
+
+for (let i = 1; i <= lastDay; i++) {
+  if (i === new Date().getDate()) {
+    days += `<div class="today">${i}</div>`;
+  } else {
+    days += `<div>${i}</div>`;
+  }
+}
+
+dayElement.innerHTML = days;
