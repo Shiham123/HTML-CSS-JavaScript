e7b1877b244e41262580b6b75521b9de335ef0e2
unknown <s36651786@gmail.com>
2023-01-03 17:21:31 +0600

    another project

--- JavaScript project ( Code With Sahand )/Animated search bar/script.js
+++ JavaScript project ( Code With Sahand )/Animated search bar/script.js
@@ -1,6 +1,5 @@
 const searchBarContainerEl = document.querySelector('.search-bar-container'),
   magnifierEl = document.querySelector('.magnifier');
-console.log(magnifierEl);
 
 magnifierEl.addEventListener('click', () => {
   searchBarContainerEl.classList.toggle('active');
