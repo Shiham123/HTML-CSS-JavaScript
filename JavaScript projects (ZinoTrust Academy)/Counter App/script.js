2edab6aba80d47509693653ab0d52c7a5ea5b71b
unknown <s36651786@gmail.com>
2023-01-03 17:28:38 +0600

    another project

---
+++ JavaScript projects (ZinoTrust Academy)/Counter App/script.js
@@ -0,0 +1,4 @@
+const countEl = document.querySelector('.count'),
+  addEl = document.querySelector('.add'),
+  resetCountEl = document.querySelector('.reset'),
+  subtractionEl = document.querySelector('.subtract');
