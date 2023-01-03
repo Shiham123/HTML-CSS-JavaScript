9e559d8a54f9de25dcca78bbbdb03b520b52f668
unknown <s36651786@gmail.com>
2023-01-03 18:32:43 +0600

    fixing the project

--- JavaScript projects (ZinoTrust Academy)/Counter App/script.js
+++ JavaScript projects (ZinoTrust Academy)/Counter App/script.js
@@ -1,4 +1,30 @@
 const countEl = document.querySelector('.count'),
   addEl = document.querySelector('.add'),
   resetCountEl = document.querySelector('.reset'),
-  subtractionEl = document.querySelector('.subtract');
+  subtractionEl = document.querySelector('.subtract'),
+  buttonsEl = document.querySelector('.buttons');
+
+buttonsEl.addEventListener('click', (event) => {
+  if (event.target.classList.contains('add')) {
+    countEl.innerHTML++;
+    setColor();
+  }
+  if (event.target.classList.contains('subtract')) {
+    countEl.innerHTML--;
+    setColor();
+  }
+  if (event.target.classList.contains('reset')) {
+    countEl.innerHTML = 0;
+    setColor();
+  }
+});
+
+function setColor() {
+  if (countEl.innerHTML > 0) {
+    countEl.style.color = 'Yellow';
+  } else if (countEl.innerHTML < 0) {
+    countEl.style.color = 'red';
+  } else {
+    countEl.style.color = 'white';
+  }
+}
