ff369ce5d0aa8cf05eac215b63393fdac3275030
unknown <s36651786@gmail.com>
2023-01-06 21:50:21 +0600

    another section

--- JavaScript projects (ZinoTrust Academy)/Weather app/script.js
+++ JavaScript projects (ZinoTrust Academy)/Weather app/script.js
@@ -13,7 +13,6 @@ function getInput(event) {
 
   if (event.type == 'click') {
     getData(searchEl.value);
-    console.log(searchEl.value);
   }
 }
 
@@ -34,11 +33,61 @@ function displayData(response) {
     errorEl.textContent = 'Please enter valid city';
     searchEl.value = '';
   } else {
+    const errorEl = document.querySelector('.error');
+    errorEl.textContent = '';
+    searchEl.value = '';
+
     const cityEl = document.querySelector('.city');
     cityEl.innerText = `${response.name}, ${response.sys.country}`;
 
     const today = new Date();
     const dateEl = document.querySelector('.date');
     dateEl.innerText = getDateFun(today);
+
+    const tempEl = document.querySelector('.temp');
+    tempEl.innerHTML = `Temp : ${Math.round(
+      response.main.temp
+    )} <span>°C</span>`;
+
+    const weatherEl = document.querySelector('.weather');
+    weatherEl.innerText = `Weather : ${response.weather[0].main}`;
+
+    const descriptionEl = document.querySelector('.description');
+    descriptionEl.innerText = `Description: ${response.weather[0].description}`;
+
+    const tempRangeEl = document.querySelector('.temp-range');
+    tempRangeEl.innerText = `Temp Range: ${Math.round(
+      response.main.temp_max
+    )} °C / ${Math.round(response.main.temp_min)} °C`;
+
+    const weatherIconEl = document.querySelector('.weather-icon');
+    const iconURL = 'http://openweathermap.org/img/w/';
+    weatherIconEl.src = iconURL + response.weather[0].icon + '.png';
   }
+}
+
+function getDateFun(today) {
+  let months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+
+  let day = days[today.getDay()];
+  let date = today.getDate();
+  let month = months[today.getMonth()];
+  let year = today.getFullYear();
+
+  return `${day}, ${date} ${month}, ${year}`;
 }
