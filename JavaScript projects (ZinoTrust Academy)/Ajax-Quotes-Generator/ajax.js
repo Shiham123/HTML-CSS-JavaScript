2ae7583045fd7861b1896254ea1f6ed13ac3db6f
unknown <s36651786@gmail.com>
2023-01-07 18:44:03 +0600

    another project

--- JavaScript projects (ZinoTrust Academy)/Ajax-Quotes-Generator/ajax.js
+++ JavaScript projects (ZinoTrust Academy)/Ajax-Quotes-Generator/ajax.js
@@ -1,31 +1,61 @@
-const btlEl = document.querySelector('.get-quotes');
+const btlEl = document.querySelector('.get-quotes'),
+  numberEl = document.getElementById('number');
 btlEl.addEventListener('click', getQuotes);
 
 function getQuotes(event) {
   event.preventDefault();
 
-  const http = new XMLHttpRequest();
+  if (numberEl.value.length == 0) {
+    return alert('Please enter a number ');
+  } else {
+    const http = new XMLHttpRequest();
 
-  http.open('GET', 'https://type.fit/api/quotes', true);
+    http.open('GET', 'https://type.fit/api/quotes', true);
+    http.onload = function () {
+      if (this.status === 200) {
+        let response = randomQuotes(JSON.parse(this.responseText)),
+          output = '';
+        // response.forEach(function (res) {
+        //   output += `
+        //       <hr>
+        //           <h5>Quote: ${res.text}</h5>
+        //           <h6>Quote: ${res.author}</h6>
+        //       <hr>
+        //       `;
+        // });
 
-  http.onload = function () {
-    if (this.status === 200) {
-      let response = JSON.parse(this.responseText),
-        output = '';
+        for (let i = 0; i < response.length; i++) {
+          if (i == numberEl.value) {
+            break;
+          }
+          output += `
+           <hr>
+               <h5>Quote: ${response[i].text}</h5>
+               <h6>Quote: ${response[i].author}</h6>
+               <hr>
+        `;
+        }
+        const quotesEl = document.querySelector('.quotes');
+        quotesEl.innerHTML = output;
+      }
+    };
+    http.send();
+  }
+}
 
-      response.forEach(function (res) {
-        output += `
-            <hr>
-                <h5>Quote: ${res.text}</h5>
-                <h6>Quote: ${res.author}</h6>
-            <hr>
-            `;
-      });
+function randomQuotes(quotes) {
+  let currentIndex = quotes.length,
+    tempValue,
+    randomIndex;
 
-      const quotesEl = document.querySelector('.quotes');
-      quotesEl.innerHTML = output;
-    }
-  };
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-  http.send();
+    tempValue = quotes[currentIndex];
+    quotes[currentIndex] = quotes[randomIndex];
+    quotes[randomIndex] = tempValue;
+  }
+
+  return quotes;
 }
