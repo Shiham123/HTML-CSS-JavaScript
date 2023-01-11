7e144c583a5484ae69cf53c7be37910d965cb784
unknown <s36651786@gmail.com>
2023-01-11 09:46:34 +0600

    another project

--- JavaScript projects (Florin Pop)/Movie App/script.js
+++ JavaScript projects (Florin Pop)/Movie App/script.js
@@ -3,4 +3,3 @@ const apiUrl =
 const imgPath = 'https://image.tmdb.org/t/p/w1280';
 const searchAPI =
   'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
-
