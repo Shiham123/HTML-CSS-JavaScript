c0c50680d365df4c0832c0f8fa1c95731c288586
Shiham Hotmail <s36651786@gmail.com>
2023-03-12 11:33:01 +0600

    scroll project still buffering

--- Javascript project (Coding Addict)/Scroll/app.js
+++ Javascript project (Coding Addict)/Scroll/app.js
@@ -1,39 +1,40 @@
-// date time is update for copyright section
+// date section
 const dateEl = document.querySelector('footer > p > span');
 dateEl.innerHTML = new Date().getFullYear();
 
-// responsive navbar fixing
-const navToggleEl = document.querySelector('.nav-toggle');
-const linksContainerEl = document.querySelector('.links-container');
-const linksEl = document.querySelector('.links');
+// responsive mood fix
+const navToggleEl = document.querySelector('.nav-toggle'),
+  linksContainerEl = document.querySelector('.links-container'),
+  linksEl = document.querySelector('.links');
 
 navToggleEl.addEventListener('click', () => {
   const containerHeight = linksContainerEl.getBoundingClientRect().height;
-  const linksHeight = linksEl.getBoundingClientRect().height;
+  const linkHeight = linksEl.getBoundingClientRect().height;
 
   if (containerHeight === 0) {
-    linksContainerEl.style.height = `${linksHeight}px`;
+    linksContainerEl.style.height = `${linkHeight}px`;
   } else {
     linksContainerEl.style.height = 0;
   }
 });
 
-// navbar fixed setup and back to top button setup
-
-const navEl = document.querySelector('#nav');
+// navbar fix
+const navbarEl = document.querySelector('#nav');
 const topLinkEl = document.querySelector('.top-link');
 
 window.addEventListener('scroll', () => {
-  const navHeight = navEl.getBoundingClientRect().height;
+  const navbarHeight = navbarEl.getBoundingClientRect().height;
   const windowHeight = window.scrollY;
 
-  if (windowHeight > navHeight) {
-    navEl.classList.add('fixed-nav');
+  if (windowHeight > navbarHeight) {
+    navbarEl.classList.add('fixed-nav');
   } else {
-    navEl.classList.remove('fixed-nav');
+    navbarEl.classList.remove('fixed-nav');
   }
 
-  if (windowHeight > 500) {
+  const topLinkHeight = topLinkEl.getBoundingClientRect().height;
+
+  if (topLinkHeight < 500) {
     topLinkEl.classList.add('show-link');
   } else {
     topLinkEl.classList.remove('show-link');
