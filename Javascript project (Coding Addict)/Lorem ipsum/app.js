f0bb44b2ff3e58251f952c42014dd1be09fda39f
Shiham Hotmail <s36651786@gmail.com>
2023-03-16 13:22:53 +0600

    lorem project done

--- Javascript project (Coding Addict)/Lorem ipsum/app.js
+++ Javascript project (Coding Addict)/Lorem ipsum/app.js
@@ -26,3 +26,26 @@ const text = [
   'y',
   'z',
 ];
+
+const loremFormEl = document.querySelector('.lorem-form'),
+  amountEl = document.getElementById('amount'),
+  loremTextEl = document.querySelector('.lorem-text');
+
+loremFormEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const value = amountEl.value;
+  const random = Math.floor(Math.random() * text.length);
+
+  if (isNaN(value) || value < 0 || value > 26) {
+    loremTextEl.innerHTML = `<p class="result">${text[random]}</p>`;
+  } else {
+    let tempText = text.slice(0, value);
+    tempText = tempText
+      .map((item) => {
+        return `<p class="result">${item.toUpperCase()}</p>`;
+      })
+      .join('');
+    loremTextEl.innerHTML = tempText;
+  }
+});
