dfee796e93f502f71fffc1b562db4609b4e18954
Shiham Hotmail <s36651786@gmail.com>
2023-03-16 13:38:52 +0600

    grocery project running

---
+++ Javascript project (Coding Addict)/Grocery bud/app.js
@@ -0,0 +1,56 @@
+const groceryFormEl = document.querySelector('.grocery-form'),
+  groceryEl = document.getElementById('grocery'),
+  alertEl = document.querySelector('.alert'),
+  groceryListEl = document.querySelector('.grocery-list'),
+  groceryContainerEl = document.querySelector('.grocery-container');
+
+let editFlag = false;
+
+groceryFormEl.addEventListener('submit', addItem);
+
+function addItem(e) {
+  e.preventDefault();
+
+  const value = groceryEl.value;
+  const id = new Date().getTime().toString();
+
+  if (value && !editFlag) {
+    const element = document.createElement('article');
+    element.classList.add('grocery-item');
+
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+
+    element.innerHTML = `
+    <p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+    `;
+
+    groceryListEl.appendChild(element);
+    groceryContainerEl.classList.add('show-container');
+    displayAlert('Item is added', 'success');
+  } else if (value && editFlag) {
+    console.log('edit item');
+  } else {
+    displayAlert('Please add a item', 'danger');
+  }
+  groceryEl.value = '';
+}
+
+function displayAlert(text, action) {
+  alertEl.textContent = text;
+  alertEl.classList.add(`alert-${action}`);
+
+  setTimeout(() => {
+    alertEl.textContent = '';
+    alertEl.classList.remove(`alert-${action}`);
+  }, 3000);
+}
