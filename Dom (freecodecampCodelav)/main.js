dc6e2335778e964d9c54fb6065db0d70eb41a5d0
Shiham Hotmail <s36651786@gmail.com>
2023-03-31 15:53:21 +0600

    clothing app running

--- Dom (freecodecampCodelav)/main.js
+++ Dom (freecodecampCodelav)/main.js
@@ -88,12 +88,15 @@ const shopData = [
   },
 ];
 
-const storeProduct = [];
+const storeProduct = JSON.parse(localStorage.getItem('data')) || [];
 
 function showProduct() {
   return (shopEl.innerHTML = shopData
     .map((item) => {
       let { id, name, price, desc, img } = item;
+      let searchLocalStorage =
+        storeProduct.find((item) => item.id === id) || [];
+
       return `
     <div id="product-${id}" class="item">
         <img width="326" src="${img}" alt="" />
@@ -105,7 +108,9 @@ function showProduct() {
             <h2>$ ${price}</h2>
             <div class="buttons">
               <i onclick="incrementProduct(${id})" class="fa-solid fa-square-plus"></i>
-              <div id="${id}" class="quantity">0</div>
+              <div id="${id}" class="quantity">${
+        searchLocalStorage.item === undefined ? 0 : searchLocalStorage.item
+      }</div>
               <i onclick="decrementProduct(${id})" class="fa-solid fa-square-minus"></i>
             </div>
           </div>
@@ -130,18 +135,24 @@ function incrementProduct(id) {
   } else {
     searchItem.item += 1;
   }
+
   updateProduct(selectItem.id);
+  localStorage.setItem('data', JSON.stringify(storeProduct));
 }
 
 function decrementProduct(id) {
   let selectItem = id;
   let searchItem = storeProduct.find((item) => item.id === selectItem.id);
 
-  if (searchItem.item === 0) return;
+  if (searchItem === undefined) return;
+  else if (searchItem.item === 0) return;
   else {
     searchItem.item -= 1;
   }
+
   updateProduct(selectItem.id);
+  storeProduct = storeProduct.filter((x) => x.item !== 0);
+  localStorage.setItem('data', JSON.stringify(storeProduct));
 }
 
 function updateProduct(id) {
@@ -154,3 +165,5 @@ function calculationProduct() {
   let updateAmount = storeProduct.map((x) => x.item).reduce((x, y) => x + y, 0);
   cartAmountEl.innerHTML = updateAmount;
 }
+
+calculationProduct();
