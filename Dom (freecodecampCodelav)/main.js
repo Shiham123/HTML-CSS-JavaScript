dca2cf69661cb9c77654a3e21563b3a88f9e6327
Shiham Hotmail <s36651786@gmail.com>
2023-04-01 16:48:19 +0600

    clothing app still running

--- Dom (freecodecampCodelav)/main.js
+++ Dom (freecodecampCodelav)/main.js
@@ -93,21 +93,21 @@ let basket = JSON.parse(localStorage.getItem('data')) || [];
 function showProduct() {
   shopEl.innerHTML = shopData
     .map((item) => {
-      let { id, price, desc, img, name } = item;
-      let showItemLocalStorage = basket.find((item) => item.id === id) || [];
+      let { id, name, price, desc, img } = item;
+      let searchItem = basket.find((item) => item.id === id) || [];
       return `
     <div id="product-${id}" class="item">
         <img width="326" src="${img}" alt="" />
         <div class="details">
           <h3>${name}</h3>
-          <p>${desc} ${id}</p>
+          <p>${desc}</p>
 
           <div class="prince-quantity">
             <h2>$ ${price}</h2>
             <div class="buttons">
               <i onclick="incrementProduct(${id})" class="fa-solid fa-square-plus"></i>
               <div id="${id}" class="quantity">${
-        showItemLocalStorage.item === undefined ? 0 : showItemLocalStorage.item
+        searchItem.item === undefined ? 0 : searchItem.item
       }</div>
               <i onclick="decrementProduct(${id})" class="fa-solid fa-square-minus"></i>
             </div>
@@ -141,11 +141,9 @@ function decrementProduct(id) {
   let selectItem = id;
   let searchItem = basket.find((item) => item.id === selectItem.id);
 
-  if (searchItem === undefined) {
-    return;
-  } else if (searchItem.item === 0) {
-    return;
-  } else {
+  if (searchItem === undefined) return;
+  else if (searchItem.item === 0) return;
+  else {
     searchItem.item -= 1;
   }
   updateProduct(selectItem.id);
@@ -160,10 +158,10 @@ function updateProduct(id) {
 }
 
 function calculation() {
-  let searchItem = basket
+  let selectItem = basket
     .map((item) => item.item)
     .reduce((itemOne, itemTwo) => itemOne + itemTwo, 0);
-  cartAmountEl.innerHTML = searchItem;
+  cartAmountEl.innerHTML = selectItem;
 }
 
 calculation();
