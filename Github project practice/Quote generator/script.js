ff5ec371f3708e2a14e6f6fb738ea1b1783271e5
Shiham123 <s36651786@gmail.com>
2023-06-15 09:34:47 +0600

    running

--- Github project practice/Quote generator/script.js
+++ Github project practice/Quote generator/script.js
@@ -5,59 +5,47 @@ const quoteContainerEl = document.getElementById('quote-container'),
   newQuoteBtnEl = document.getElementById('new-quote'),
   loaderEl = document.getElementById('loader');
 
-let apiQuotes = [];
+let apiQuotes = [],
+  apiUrl = `https://type.fit/api/quotes`;
 
 newQuoteBtnEl.addEventListener('click', newQuote);
 twitterBtnEl.addEventListener('click', twitterQuote);
 
-function showLoadingSpinner() {
+function showSpinner() {
   loaderEl.hidden = false;
   quoteContainerEl.hidden = true;
 }
 
-function removeLoadingSpinner() {
-  loaderEl.hidden = true;
-  quoteContainerEl.hidden = false;
+function removeSpinner() {
+  setInterval(() => {
+    loaderEl.hidden = true;
+    quoteContainerEl.hidden = false;
+  }, 1000);
 }
 
 function newQuote() {
-  showLoadingSpinner();
-
+  showSpinner();
   const quotes = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
-  if (!quotes.author) {
-    authorEl.innerText = 'unknown';
-  } else {
-    authorEl.innerText = quotes.author;
-  }
-
-  if (quotes.text.length > 50) {
-    quoteTextEl.classList.add('long-quote');
-  } else {
-    quoteTextEl.classList.remove('long-quote');
-  }
-
-  quoteTextEl.innerText = quotes.text;
-  removeLoadingSpinner();
-}
-
-async function getQuote() {
-  showLoadingSpinner();
-  const apiURL = 'https://type.fit/api/quotes';
-
-  try {
-    const response = await fetch(apiURL);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch (error) {
-    console.log('whoops, no quote', error);
-    getQuote();
-  }
+  authorEl.innerText = !quotes.author ? 'Unknown' : quotes.author;
+  quoteTextEl.innerText = !quotes.text ? 'Unknown' : quotes.text;
+  removeSpinner();
 }
 
 function twitterQuote() {
-  const twitterURL = `https://twitter.com/intent/tweet?text=${quoteTextEl.textContent} - ${authorEl.textContent}`;
-  window.open(twitterURL, '_blank');
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteTextEl.textContent} - ${authorEl.textContent}`;
+  window.open(twitterUrl, '_blank');
 }
 
-getQuote();
+async function getQuotes() {
+  try {
+    let response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    console.log('whoops something out there', error);
+    getQuotes();
+  }
+}
+
+getQuotes();
