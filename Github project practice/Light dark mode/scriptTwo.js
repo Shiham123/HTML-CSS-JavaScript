2e56cace277f1ac79703dea04f016e22a790b5dd
Shiham123 <s36651786@gmail.com>
2023-06-16 11:46:11 +0600

    coding practice

--- Github project practice/Light dark mode/scriptTwo.js
+++ Github project practice/Light dark mode/scriptTwo.js
@@ -14,22 +14,20 @@ function imageMode(color) {
   imageThreeEl.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-function toggleDarkLightMode(isDark) {
-  navEl.style.backgroundColor = isDark
-    ? 'rgba(0 0 0 / 50%)'
-    : 'rgba(255 255 255 / 50% )';
+function darkMode() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode('dark');
+}
 
-  textBoxEl.style.backgroundColor = isDark
-    ? 'rgb(255 255 255 / 50%)'
-    : 'rgb(0 0 0 /50%)';
-
-  toggleIconEl.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
-
-  isDark
-    ? toggleIconEl.children[1].classList.replace('fa-sun', 'fa-moon')
-    : toggleIconEl.children[1].classList.replace('fa-moon', 'fa-sun');
-
-  isDark ? imageMode('dark') : imageMode('light');
+function lightMode() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  imageMode('light');
 }
 
 function switchTheme(event) {
