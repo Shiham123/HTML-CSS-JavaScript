7b41cb337132e36ac51298eef6a0e98b9b08d9f1
Shiham123 <s36651786@gmail.com>
2023-06-17 09:13:27 +0600

    project dark light is complete

--- Github project practice/Light dark mode/script.js
+++ Github project practice/Light dark mode/script.js
@@ -1,55 +1,57 @@
-const toggleSwitchEl = document.querySelector('input[type="checkbox"]'),
+const toggleIconEl = document.getElementById('toggle-icon'),
+  inputEl = document.querySelector('input[type="checkbox"]'),
   navEl = document.getElementById('nav'),
-  toggleIconEl = document.getElementById('toggle-icon'),
-  imageOneEl = document.getElementById('image1'),
-  imageTwoEl = document.getElementById('image2'),
-  imageThreeEl = document.getElementById('image3'),
+  imgOneEl = document.getElementById('image1'),
+  imgTwoEl = document.getElementById('image2'),
+  imgThreeEl = document.getElementById('image3'),
   textBoxEl = document.getElementById('text-box');
 
-toggleSwitchEl.addEventListener('click', switchTheme);
-
-function imageMode(color) {
-  imageOneEl.src = `img/undraw_proud_coder_${color}.svg`;
-  imageTwoEl.src = `img/undraw_feeling_proud_${color}.svg`;
-  imageThreeEl.src = `img/undraw_conceptual_idea_${color}.svg`;
-}
-
-function toggleDarkLightMode(isDark) {
-  navEl.style.backgroundColor = isDark
-    ? 'rgba(0 0 0 / 50%)'
-    : 'rgba(255 255 255 / 50% )';
-
-  textBoxEl.style.backgroundColor = isDark
-    ? 'rgb(255 255 255 / 50%)'
-    : 'rgb(0 0 0 /50%)';
-
-  toggleIconEl.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
-
-  isDark
-    ? toggleIconEl.children[1].classList.replace('fa-sun', 'fa-moon')
-    : toggleIconEl.children[1].classList.replace('fa-moon', 'fa-sun');
-
-  isDark ? imageMode('dark') : imageMode('light');
-  console.log(isDark);
-}
+inputEl.addEventListener('click', switchTheme);
 
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
-    toggleDarkLightMode(true);
+    toggleSwitchTheme(true);
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
-    toggleDarkLightMode(false);
+    toggleSwitchTheme(false);
   }
+}
+
+function toggleSwitchTheme(isMode) {
+  navEl.style.backgroundColor = isMode
+    ? 'rgb(0 0 0 / 50%)'
+    : 'rgb(255 255 255 / 50%)';
+
+  textBoxEl.style.backgroundColor = isMode
+    ? 'rgb(255 255 255 / 50%)'
+    : 'rgb(0 0 0 / 50%)';
+
+  toggleIconEl.children[0].textContent = isMode ? 'Dark Mode' : 'Light Mode';
+
+  toggleIconEl.children[1].classList.replace(
+    isMode ? 'fa-sun' : 'fa-moon',
+    isMode ? 'fa-moon' : 'fa-sun'
+  );
+
+  imageMode(isMode ? 'dark' : 'light');
+}
+
+function imageMode(color) {
+  imgOneEl.src = `img/undraw_proud_coder_${color}.svg`;
+  imgTwoEl.src = `img/undraw_feeling_proud_${color}.svg`;
+  imgThreeEl.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
 const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
+  toggleSwitchTheme(false);
   if (currentTheme === 'dark') {
-    toggleSwitchEl.checked = true;
+    inputEl.checked = true;
+    toggleSwitchTheme(true);
   }
 }
--- Github project practice/Light dark mode/scriptTwo.js
+++ Github project practice/Light dark mode/scriptTwo.js
@@ -1,40 +1,12 @@
-const toggleSwitchEl = document.querySelector('input[type="checkbox"]'),
+const toggleIconEl = document.getElementById('toggle-icon'),
+  inputEl = document.querySelector('input[type="checkbox"]'),
   navEl = document.getElementById('nav'),
-  toggleIconEl = document.getElementById('toggle-icon'),
   imageOneEl = document.getElementById('image1'),
   imageTwoEl = document.getElementById('image2'),
   imageThreeEl = document.getElementById('image3'),
   textBoxEl = document.getElementById('text-box');
 
-toggleSwitchEl.addEventListener('click', switchTheme);
-
-function imageMode(color) {
-  imageOneEl.src = `img/undraw_proud_coder_${color}.svg`;
-  imageTwoEl.src = `img/undraw_feeling_proud_${color}.svg`;
-  imageThreeEl.src = `img/undraw_conceptual_idea_${color}.svg`;
-}
-
-function darkMode() {
-  navEl.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  textBoxEl.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-
-  toggleIconEl.children[0].textContent = 'Dark Mode';
-  toggleIconEl.children[1].classList.replace('fa-sun', 'fa-moon');
-  toggleIconEl.children[1].style.transition = '0.4s';
-
-  imageMode('dark');
-}
-
-function lightMode() {
-  navEl.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  textBoxEl.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-
-  toggleIconEl.children[0].textContent = 'Light Mode';
-  toggleIconEl.children[1].classList.replace('fa-moon', 'fa-sun');
-  toggleIconEl.children[1].style.transition = '0.4s';
-
-  imageMode('light');
-}
+inputEl.addEventListener('click', switchTheme);
 
 function switchTheme(event) {
   if (event.target.checked) {
@@ -48,12 +20,37 @@ function switchTheme(event) {
   }
 }
 
+function imageMode(color) {
+  imageOneEl.src = `img/undraw_proud_coder_${color}.svg`;
+  imageTwoEl.src = `img/undraw_feeling_proud_${color}.svg`;
+  imageThreeEl.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+function darkMode() {
+  navEl.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBoxEl.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+
+  toggleIconEl.children[0].textContent = 'Dark Mode';
+  toggleIconEl.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode('dark');
+}
+
+function lightMode() {
+  navEl.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBoxEl.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+
+  toggleIconEl.children[0].textContent = 'Light Mode';
+  toggleIconEl.children[1].classList.replace('fa-moon', 'fa-sun');
+  imageMode('light');
+}
+
 const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
   if (currentTheme === 'dark') {
-    toggleSwitchEl.checked = true;
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    inputEl.checked = true;
     darkMode();
   }
 }
