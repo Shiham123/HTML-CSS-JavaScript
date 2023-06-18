e295ff61a0ec20bda4423c2eabbba1d685dfd8cc
Shiham123 <s36651786@gmail.com>
2023-06-18 22:29:58 +0600

    running on practice

--- Github project practice/Book keeper/script.js
+++ Github project practice/Book keeper/script.js
@@ -6,8 +6,13 @@ const modalEl = document.getElementById('modal'),
   websiteUrlEl = document.getElementById('website-url'),
   bookmarkContainerEl = document.getElementById('bookmarks-container');
 
+bookmarkFormEl.addEventListener('submit', storeBookmark);
+
+let bookmarks = [];
+
 showModalEl.addEventListener('click', showModal);
 closeModalEl.addEventListener('click', closeModal);
+
 window.addEventListener('click', (event) => {
   event.target === modalEl && modalEl.classList.remove('show-modal');
 });
@@ -19,4 +24,56 @@ function showModal() {
 
 function closeModal() {
   modalEl.classList.remove('show-modal');
+}
+
+function storeBookmark(event) {
+  event.preventDefault();
+
+  let nameValue = websiteNameEl.value,
+    urlValue = websiteUrlEl.value;
+
+  if (urlValue.includes('http://', 'https://')) {
+    urlValue = `https//${urlValue}`;
+  }
+
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+
+  bookmarks.push(bookmark);
+  localStorage.setItem('bookmark', JSON.stringify(bookmark));
+  fetchBookmarks();
+}
+
+function validate(nameValue, urlValue) {
+  const expression =
+    /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+
+  const regex = new RegExp(expression);
+
+  if (!nameValue || !urlValue) {
+    alert('please submit value for both fields');
+    return false;
+  }
+
+  if (!urlValue.match(regex)) {
+    alert('please provide a correct website address');
+    return false;
+  }
+
+  return true;
+}
+
+function fetchBookmarks() {
+  if (localStorage.getItem('bookmark')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmark'));
+  } else {
+    bookmarks = [{ name: 'shiham', url: 'https://www.facebook.com/' }];
+    localStorage.setItem('bookmark', JSON.stringify(bookmarks));
+  }
 }
