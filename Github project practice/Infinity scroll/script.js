e6beae8bf1e29d293d47d8f209d306890d827f59
Shiham123 <s36651786@gmail.com>
2023-06-18 10:20:53 +0600

    another project start

--- Github project practice/Infinity scroll/script.js
+++ Github project practice/Infinity scroll/script.js
@@ -5,16 +5,57 @@ const initialCount = 5,
   apiKey = '-KsOXvdzrmis5K5FFHbUNb1HOJks2OpVmYARueRdjAw',
   apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${initialCount}`;
 
-let photosArray = [];
+let photosArray = [],
+  imagesLoaded = 0,
+  totalImages = 0;
 
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
-    console.log(photosArray);
+    displayPhotos();
   } catch (error) {
     console.log(error);
   }
 }
 
-window.addEventListener('scroll', () => {});
+function setAttributeEl(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log(photosArray);
+
+  photosArray.forEach((photo) => {
+    const image = document.createElement('img'),
+      item = document.createElement('a');
+
+    setAttributeEl(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+
+    setAttributeEl(image, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    item.appendChild(image);
+    imgContainerEl.appendChild(item);
+
+    image.addEventListener('load', imageLoaded);
+  });
+}
+
+function imageLoaded() {
+  console.log('hlw');
+}
+
+window.addEventListener('scroll', () => {
+  getPhotos();
+});
