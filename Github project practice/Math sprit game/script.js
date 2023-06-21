694da50ad4ecbb3f403254f0c734395b1dd04620
Shiham123 <s36651786@gmail.com>
2023-06-21 12:40:10 +0600

    overwhemming

--- Github project practice/Math sprit game/script.js
+++ Github project practice/Math sprit game/script.js
@@ -16,3 +16,72 @@ const finalTimeEl = document.querySelector('.final-time'),
   baseTimeEl = document.querySelector('.base-time'),
   penaltyTimeEl = document.querySelector('.penalty-time'),
   playAgainBtnEl = document.querySelector('.play-again');
+
+let questionAmount = 0;
+
+startFormEl.addEventListener('submit', startQuestionAmount);
+startFormEl.addEventListener('click', () => {
+  radioContainerEl.forEach((radioEl) => {
+    radioEl.classList.remove('selected-label');
+
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add('selected-label');
+    }
+  });
+});
+
+function startQuestionAmount(event) {
+  event.preventDefault();
+  questionAmount = getRadioValue();
+  // console.log('question amount', questionAmount);
+  if (questionAmount) {
+    showCountdown();
+  }
+}
+
+function getRadioValue() {
+  let radioValue;
+  radioInputEl.forEach((input) => {
+    if (input.checked) {
+      radioValue = input.value;
+    }
+  });
+  return radioValue;
+}
+
+function showCountdown() {
+  countdownPageEl.hidden = false;
+  splashPageEl.hidden = true;
+  countdownStart();
+  populateGamePage();
+}
+
+function countdownStart() {
+  countdownEl.textContent = '3';
+  setTimeout(() => {
+    countdownEl.textContent = '2';
+  }, 1000);
+  setTimeout(() => {
+    countdownEl.textContent = '1';
+  }, 2000);
+  setTimeout(() => {
+    countdownEl.textContent = 'GO';
+  }, 3000);
+}
+
+function populateGamePage() {
+  itemContainerEl.textContent = '';
+
+  const topScorer = document.createElement('div');
+  topScorer.classList.add('height-240');
+
+  const selectedItem = document.createElement('div');
+  selectedItem.classList.add('selected-item');
+
+  itemContainerEl.append(topScorer, selectedItem);
+
+  const bottomSpacer = document.createElement('div');
+  bottomSpacer.classList.add('height-500');
+
+  itemContainerEl.appendChild(bottomSpacer);
+}
