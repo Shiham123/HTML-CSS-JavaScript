2c908ed3b2e9d9b4927f59028b80c6ae691606bb
Shiham123 <s36651786@gmail.com>
2023-06-23 08:02:20 +0600

    nasa project

--- Github project practice/NASA/script.js
+++ Github project practice/NASA/script.js
@@ -29,11 +29,58 @@ function createDOMNodes(page) {
     page === 'results' ? resultArray : Object.values(favorites);
 
   currentArray.forEach((result) => {
-    console.log(result);
     const card = document.createElement('div');
     card.classList.add('card');
 
     const link = document.createElement('a');
+    link.href = result.hdurl;
+    link.title = 'View Full Image';
+    link.target = '_blank';
+
+    const image = document.createElement('img');
+    image.src = result.url;
+    image.alt = 'NASA Pictures of the day';
+    image.loading = 'lazy';
+    image.classList.add('card-img-top');
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-title');
+
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = result.title;
+
+    const saveText = document.createElement('p');
+    saveText.classList.add('clickable');
+
+    if (page === 'results') {
+      saveText.textContent = 'ADD to favorites';
+      saveText.setAttribute('onclick', `saveFavorites('${result.url}')`);
+    } else {
+      saveText.textContent = 'Remove favorites';
+      saveText.setAttribute('onclick', `removeFavorites('${result.url}')`);
+    }
+
+    const cardText = document.createElement('p');
+    cardText.textContent = result.explanation;
+
+    const footer = document.createElement('small');
+    footer.classList.add('text-muted');
+
+    const date = document.createElement('strong');
+    date.textContent = result.date;
+
+    const copyRightResult =
+      result.copyright === undefined ? '' : result.copyright;
+
+    const copyRight = document.createElement('span');
+    copyRight.textContent = `${copyRightResult}`;
+
+    footer.append(date, copyRight);
+    cardBody.append(cardTitle, saveText, cardText, footer);
+    link.appendChild(image);
+    card.append(link, cardBody);
+    imageContainerEl.appendChild(card);
   });
 }
 
