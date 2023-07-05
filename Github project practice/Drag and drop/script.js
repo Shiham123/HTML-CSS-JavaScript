90007cde33968d92cd71536bb64bab54d729740b
Shiham123 <s36651786@gmail.com>
2023-07-05 16:16:26 +0600

    another

---
+++ Github project practice/Drag and drop/script.js
@@ -0,0 +1,10 @@
+const addBtnEl = document.querySelectorAll('.add-btn:not(.solid)'),
+  saveItemBtn = document.querySelectorAll('.solid'),
+  addContainerEl = document.querySelectorAll('.add-container'),
+  addItemEl = document.querySelectorAll('.add-item');
+
+const listColumnsEl = document.querySelectorAll('.drag-item-list'),
+  backlogListEl = document.getElementById('backlog-list'),
+  progressListEl = document.getElementById('progress-list'),
+  completeListEl = document.getElementById('complete-list'),
+  holdListEl = document.getElementById('on-hold-list');
